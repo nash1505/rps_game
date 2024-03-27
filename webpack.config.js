ae769ec9eac8b18342fe5b6e6677117661d679e1
Nachiket Gujalwar <60006574+nash1505@users.noreply.github.com>
2024-03-27 15:15:18 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:8000/",
+    publicPath: "/",
   },
 
   resolve: {
